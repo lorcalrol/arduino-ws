f538f582be05bc49a5a1ecdd9eefd48d270a3841
Lorena <lorenacalvente@gmail.com>
2019-03-30 13:31:00 +0100

    Correct ChipId (0x60)

--- ComunicacionWire/ComunicacionWire.ino
+++ ComunicacionWire/ComunicacionWire.ino
@@ -10,23 +10,28 @@ void setup() {
 
 void loop() {
   // request 6 bytes from slave device #8
+  
+  Wire.beginTransmission(0x76);
+  Wire.write(0xD0);
   Wire.requestFrom(0x76, 1); // EE ó EC
-
-  // slave may send less than requested
-  while (Wire.available())
-  {
-    // receive a byte as character
-    char c = Wire.read();
-    char d = ESP.getChipId();
+  char c = Wire.read();
       
-    // print the character
-    Serial.printf("Received: %02X\n", c);
-    Serial.printf("Received: %02X\n", d);
-    Serial.printf("Hello world!\n");
-    Serial.print(d);
-
-  }
-
-  Serial.printf("Hello world2!\n");
+  // print the character
+  Serial.printf("Received: %02X\n", c);
+  
   delay(500);
+  
+  Wire.endTransmission();
+//  // slave may send less than requested
+//  while (Wire.available())
+//  {
+//    // receive a byte as character
+//    char c = Wire.read();
+//      
+//    // print the character
+//    Serial.printf("Received: %02X\n", c);
+//    Serial.printf("Hello world!\n");
+
+//  }
+
 }
