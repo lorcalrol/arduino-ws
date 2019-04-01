d143345a5e8502d649c51ab0a1dba417da843663
Lorena <lorenacalvente@gmail.com>
2019-04-01 23:18:07 +0200

    Humidity 1

--- ComunicacionWire/ComunicacionWire.ino
+++ ComunicacionWire/ComunicacionWire.ino
@@ -1,31 +1,54 @@
+/* Include required headers and/or libraries */
 #include <Wire.h>
 
-void setup() {
-  // join i2c bus (address optional for master)
-  Wire.begin(0, 2);// GND
+/* Single-pass function to configure the app */
+void setup()
+{
+  /* Join i2c bus (address optional for master) */
+  Wire.begin(0, 2);
 
-  // start serial for output
+  /* start serial for output */
   Serial.begin(115200);
 }
 
-void loop() {
+/* Recurrent task, called forever */
+void loop()
+{
+  /* Welcome message! Useful to know where  */
+  Serial.printf("Ahoy! ESP8266 here!\n---\n");
+
+ /* Begin transmission */
+  Wire.beginTransmission(0x76); 
   
-  Wire.beginTransmission(0x76); // start a connection with slave device
-  Wire.write(0xFD);             // select the register address
+  /* chipID register*/
+  Wire.write(0xF2); 
+  Wire.write(B001);
+
+  Wire.write(0xF4);
+  Wire.write(B00100111);
+
+  Wire.write(0xFD);
+  /* End transmission */
   Wire.endTransmission();
   
-  Wire.requestFrom(0x76, 2);    // request 2 bytes from slave device #0x76
+  /* Request data from slave with address 0x76 */
+  Wire.requestFrom(0x76, 2); 
 
-  // slave may send less than requested
+  /* Wait for data to be available */
   while (Wire.available())
   {
-    // receive a byte as character
-    uint8_t hum1 = Wire.read();   // read
-    uint8_t hum2 = Wire.read();   // read the following register address
+    /* Receive the byte */
+    uint16_t hum1 = Wire.read();  
+    uint8_t hum2 = Wire.read();
 
-    // print the character
-    Serial.printf("Received: %u %u \n", hum1, hum2);
-  } 
-  delay(500);
+    uint16_t humtot=hum1<<8+hum2;
+    
+    /* Send it to console/monitor */
+    Serial.printf("Received: %u %u\n", hum1,hum2);
+    Serial.printf("Received: %u\n", humtot);
+
+  }
   
+  /* Ensure not to flood with a huge amount of fast data */
+  delay(500);
 }
